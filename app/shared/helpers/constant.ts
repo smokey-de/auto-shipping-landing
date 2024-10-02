002bd0b89efb7447904c15bafdd5aba0f0f07a26
Abrorxon <abror.karimxonov@ssd.uz>
2024-10-02 11:03:23 +0500

    changed email account key

--- app/shared/helpers/constant.ts
+++ app/shared/helpers/constant.ts
@@ -1,7 +1,7 @@
 const SERVICE_ID = "service_9ln609k";
-const MAIN_TEMPLATE_ID = "template_5xgj3eb";
-export const BONUS_TEMPLATE_ID = "template_a5s5v0q";
-const PUBLIC_ID = "DIHOsLaITjKv7Mq6C";
+const MAIN_TEMPLATE_ID = "template_5q7gwlc";
+export const BONUS_TEMPLATE_ID = "template_zuajl4j";
+const PUBLIC_ID = "X-N16IA5aGYbDHscC";
 
 export const emailSchema = {
   service_id: SERVICE_ID,
